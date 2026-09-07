2fb191d413c49ee304812634966a29bdf5401fe7
Gabe Fronk <gabefronk@gmail.com>
2026-09-07 02:27:41 +0000

    External agent changes

--- base44/functions/windowQuotesDraft/entry.ts
+++ base44/functions/windowQuotesDraft/entry.ts
@@ -1,6 +1,6 @@
 import { createClientFromRequest } from "npm:@base44/sdk@0.8.46";
 import { createQuoteHandler } from "../../shared/windowQuotesCore.js";
-import { execution } from "../../shared/windowQuoteAgentRuntime.js";
+import { execution } from "../../shared/windowQuoteScriptedRuntime.js";
 
 const handleQuote = createQuoteHandler({ getClient: createClientFromRequest, executionService: execution });
 
