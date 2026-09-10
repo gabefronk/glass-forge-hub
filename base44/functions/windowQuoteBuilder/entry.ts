d555d102b062a0d924e58341ae21103411b667bd
Gabe Fronk <gabefronk@gmail.com>
2026-09-10 10:09:16 +0000

    External agent changes

--- base44/functions/windowQuoteBuilder/entry.ts
+++ base44/functions/windowQuoteBuilder/entry.ts
@@ -1,5 +1,5 @@
 import { createClientFromRequest } from 'npm:@base44/sdk@0.8.46';
 import { createWindowQuoteBuilderHandler } from '../../shared/windowQuoteBuilder.js';
 import { conversationalIntake } from '../../shared/windowQuoteScriptedRuntime.js';
-// Builder intake v12: AMSCO configurator, independent line prices and native/online routing.
+// Builder intake v13: AMSCO configurator with verified warm engine contract fc3ed293.
 export default createWindowQuoteBuilderHandler({ getClient: createClientFromRequest, normalizeAI: conversationalIntake });
