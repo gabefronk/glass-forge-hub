e004dec4c6a599a0b5804c980c3b949680d5e918
Gabe Fronk <gabefronk@gmail.com>
2026-09-10 08:03:44 +0000

    External agent changes

--- base44/functions/salesTrackerAppend/entry.ts
+++ base44/functions/salesTrackerAppend/entry.ts
@@ -1,4 +1,5 @@
 import { createClientFromRequest } from "npm:@base44/sdk";
 import * as XLSX from "npm:xlsx@0.18.5";
 import { createTrackerAppendHandler } from "../../shared/salesTrackerAppendService.js";
+// Append-only import v1: shared parser and immutable batch storage.
 Deno.serve(createTrackerAppendHandler({getClient:createClientFromRequest,XLSX}));
