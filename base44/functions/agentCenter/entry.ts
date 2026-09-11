23657905cb59dc7a2fde82d0817d77a9ab2d9d2c
Gabe Fronk <gabefronk@gmail.com>
2026-09-11 15:53:13 +0000

    External agent changes

--- base44/functions/agentCenter/entry.ts
+++ base44/functions/agentCenter/entry.ts
@@ -1,3 +1,4 @@
 import { createClientFromRequest } from "npm:@base44/sdk";
 import { createAgentCenterHandler } from "../../shared/agentCenter.js";
+// Register inactive ProBuild iPad Capture design; device and email execution remain disabled.
 Deno.serve(createAgentCenterHandler({getClient:createClientFromRequest}));
